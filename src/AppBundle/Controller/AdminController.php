f40018ed9d88bb3ae8599f4f1dbb60c9b2cc57de
ptyszer <p.tyszer@hotmail.com>
2018-05-18 18:54:37 +0200

    Add admin panel

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     * @Method({"GET", "POST"})
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function adminAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $artists = $em->getRepository('AppBundle:Artist')->findBy(['approved' => 0]);
+        $albums = $em->getRepository('AppBundle:Album')->findBy(['approved' => 0]);
+        $genres = $em->getRepository('AppBundle:Genre')->findBy(['approved' => 0]);
+        return $this->render('admin/admin.html.twig', array(
+            'artists' => $artists,
+            'albums' => $albums,
+            'genres' => $genres,
+        ));
+    }
+
+}
